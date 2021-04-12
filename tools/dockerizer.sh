eb954d961929807876eff2ff7e824a56785f0956
totu <topi@topituulensuu.com>
2021-04-12 12:22:42 +0300

    improve dockerizer
    - build if no image
    - build if Dockerfile is newer than build tag
    - fix --name flag handling

--- tools/dockerizer.sh
+++ tools/dockerizer.sh
@@ -8,9 +8,10 @@ while test $# -gt 0; do
             ;;
         -r|--rebuild)
             shift
-            REBUILD=1
+            BUILD=1
             ;;
         -n|--name)
+            shift
             NAME="$1"
             shift
             ;;
@@ -23,8 +24,19 @@ done
 # Set default name if not given
 [ -z $NAME ] && NAME=mysummerjob
 
+# Check if image has been created
+[ $(docker image inspect $NAME 2> /dev/null | grep LastTagTime -c) -eq 0 ] && BUILD=1
+
+if [ ! $BUILD ]; then
+    # Check if Dockerfile is newer than image
+    LAST_IMAGE_TAG_DATE=$(date --date $(docker image inspect $NAME 2> /dev/null | grep LastTagTime | cut --delimiter='"' --fields=4) +%s)
+    # git %at == author date, UNIX timestamp
+    DOCKERFILE_MODIFIED_DATE=$(git log -1 --format=%at -- Dockerfile)
+    BUILD=$((LAST_IMAGE_TAG_DATE < DOCKERFILE_MODIFIED_DATE))
+fi
+
 # Rebuild container if --rebuild flag was given
-[ $REBUILD ] && docker image build -t $NAME .
+[ $BUILD -ne 0 ] && docker image build -t $NAME .
 
 OLDPATH=$PWD
 cd $(git rev-parse --show-toplevel)
