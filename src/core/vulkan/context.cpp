f9e0bb61fbe4e3ec86aca2942e8e385a6e8d5646
Anssi Walder <anssiwa@gmail.com>
2021-04-20 13:19:20 +0300

    imgui: use imgui docking branch

--- src/core/vulkan/context.cpp
+++ src/core/vulkan/context.cpp
@@ -213,19 +213,92 @@ void Context::updateOverlay(float dt)
             2.0f / io.DisplaySize.x, 2.0f / io.DisplaySize.y);
     m_ui->pushConstants.translate = glm::vec2(-1.0f);
 
+    bool p_open = true;
+
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    // ImGui::Begin("Settings");
-    ImGui::ShowDemoWindow();
+    {
+        static bool opt_fullscreen_persistant = true;
+        bool opt_fullscreen = opt_fullscreen_persistant;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+        dockspace_flags |= ImGuiDockNodeFlags_PassthruCentralNode;
+
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent
+        // window not dockable into, because it would be confusing to have two
+        // docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar
+                                        | ImGuiWindowFlags_NoDocking;
+        if(opt_fullscreen)
+        {
+            ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->Pos);
+            ImGui::SetNextWindowSize(viewport->Size);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar
+                            | ImGuiWindowFlags_NoResize
+                            | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus
+                            | ImGuiWindowFlags_NoNavFocus;
+            // window_flags |= ImGuiWindowFlags
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will
+        // render our background and handle the pass-thru hole, so we ask
+        // Begin() to not render a background.
+        if(dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka
+        // window is collapsed). This is because we want to keep our DockSpace()
+        // active. If a DockSpace() is inactive, all active windows docked into
+        // it will lose their parent and become undocked. We cannot preserve the
+        // docking relationship between an active window and an inactive
+        // docking, otherwise any change of dockspace/settings would lead to
+        // windows being stuck in limbo and never being visible.
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("###DockSpace", &p_open, window_flags);
+        ImGui::PopStyleVar();
+
+        if(opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if(io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+        else
+        {
+            // ShowDockingDisabledMessage();
+        }
+
+        ImGui::End();
+    }
+
+    // ImGui::ShowDemoWindow();
     // ImGui::SetWindowSize(windowSize);
     // ImGui::SetWindowPos(windowPos);
 
-    // ImGui::Text("%.3f ms/frame", 1000.0f / io.Framerate);
+    ImGui::Begin("Settings1");
+    ImGui::Text("%.3f ms/frame", 1000.0f / io.Framerate);
+    ImGui::End();
 
-    // ImGui::ColorEdit3("Color", &ubo.color.x);
+    ImGui::Begin("Settings2");
+    ImGui::Text("%.3f ms/frame", 1000.0f / io.Framerate);
+    ImGui::End();
 
-    // ImGui::End();
+    ImGui::Begin("Settings3");
+    ImGui::Text("%.3f ms/frame", 1000.0f / io.Framerate);
+    ImGui::End();
+
+    // ImGui::ColorEdit3("Color", &color);
+
+
     ImGui::Render();
 
     m_ui->update();
